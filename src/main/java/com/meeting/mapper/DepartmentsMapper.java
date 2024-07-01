3f81af9243e539d09652d597de16db38b3a10e67
shanmingxi <1827654677@qq.com>
2024-07-01 18:54:47 +0800

    s

---
+++ src/main/java/com/meeting/mapper/DepartmentsMapper.java
@@ -0,0 +1,19 @@
+package com.meeting.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.meeting.domain.Departments;
+
+/**
+* @author shanmingxi
+* @description 针对表【departments】的数据库操作Mapper
+* @createDate 2024-07-01 16:45:07
+* @Entity generator.domain.Departments
+*/
+public interface DepartmentsMapper extends BaseMapper<Departments> {
+
+}
+
+
+
+
